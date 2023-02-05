a49c131e9c01e4ed1b209f9678373e46a8bb4573
Diego Velasquez <contacto@diegovelasquez.co>
2023-02-05 01:34:32 -0500

    small updates

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -6,31 +6,28 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
-  const { prompt, size } = req.body;
 
-  const imageSize =
-    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+  const { prompt, size } = req.body;
 
   try {
     const response = await openai.createImage({
-      prompt,
+      prompt: prompt,
       n: 1,
-      size: imageSize,
+      size: '512x512',
     });
 
     const imageUrl = response.data.data[0].url;
-
     res.status(200).json({
       success: true,
       data: imageUrl,
     });
   } catch (error) {
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-    } else {
-      console.log(error.message);
-    }
+    // if (error.response) {
+    //   console.log(error.response.status);
+    //   console.log(error.response.data);
+    // } else {
+    //   console.log(error.message);
+    // }
 
     res.status(400).json({
       success: false,
